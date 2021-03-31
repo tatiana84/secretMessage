666d5efba3e7247865ecf949d075a4f4ddd1b52d
tatiana84 <tantamuntean2018@gmail.com>
2021-04-01 00:14:05 +0300

    First commit

---
+++ app.js
@@ -0,0 +1,27 @@
+let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 
+'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 
+'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
+console.log(secretMessage.length);
+
+let newSecretMessage = secretMessage.pop(); // 2 step
+//console.log(newSecretMessage);
+console.log(secretMessage.length);
+
+let newStringSecretMessage = secretMessage.push('to', 'Program'); // 3 step
+console.log(secretMessage);
+
+secretMessage[7] = 'right'; //4 step
+console.log(secretMessage);
+
+let removeItemSecretMessage = secretMessage.shift(); // 5 step
+console.log(secretMessage);
+
+let beggingAddSecretMessage = secretMessage.unshift('Programming'); // 6 step
+console.log(secretMessage);
+
+let removeStringSecretMessage = secretMessage.splice(6, 11, 'know'); // 7 step
+console.log(secretMessage);
+
+console.log(secretMessage.join()); // 8 step 
+//Programming,is,not,about,what,you,know,figure,out.,-2015,,Chris,Pine,,Learn,to,Program
+
