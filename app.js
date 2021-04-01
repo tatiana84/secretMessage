3b82349069a96011a320d16ace45b9c78be41456
tatiana84 <tantamuntean2018@gmail.com>
2021-04-02 00:35:48 +0300

    Second commit

--- app.js
+++ app.js
@@ -22,6 +22,6 @@ console.log(secretMessage);
 let removeStringSecretMessage = secretMessage.splice(6, 11, 'know'); // 7 step
 console.log(secretMessage);
 
-console.log(secretMessage.join()); // 8 step 
+console.log(secretMessage.join(' ')); // 8 step 
 //Programming,is,not,about,what,you,know,figure,out.,-2015,,Chris,Pine,,Learn,to,Program
-
+//Programming is not about what you know figure out. -2015, Chris Pine, Learn to Program
